71917219cda9d7e6097cbc092f6a2ec2d8d19554
Dillon <dillon.embreus@gmail.com>
2022-09-02 14:03:08 -0600

    Added carousel module from another project.

--- projects/carousel/src/app/app-routing.module.ts
+++ projects/carousel/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarouselComponent } from './modules/carousel/carousel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/carousel/carousel.module').then(
+        (m) => m.CarouselModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
