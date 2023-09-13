897e1ed9305e4d8ae7d65d2a96053e8d0a194b20
Dillon Embreus <dillon.embreus@protonmail.com>
2023-09-13 12:24:10 -0600

    feat: Added meta data for expertise page

--- src/app/pages/expertise/expertise.component.ts
+++ src/app/pages/expertise/expertise.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-expertise',
   templateUrl: './expertise.component.html',
-  styleUrls: ['./expertise.component.scss']
+  styleUrls: ['./expertise.component.scss'],
 })
 export class ExpertiseComponent implements OnInit {
-
-  constructor() { }
+  constructor(private metaTagService: Meta) {}
 
   ngOnInit(): void {
+    this.metaTagService.addTags([
+      {
+        name: 'description',
+        content: "Dillon Embreus's Expertise.",
+      },
+      {
+        name: 'keywords',
+        content: 'Dillon Embreus, Embreus, Portfolio, Expertise, Resume',
+      },
+    ]);
   }
-
 }
